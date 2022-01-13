e7e7e205b6e9ca57d91a95f9a8979946633e312e
tika.kristiani <tika.kristiani@gmail.com>
2022-01-13 11:09:45 +0700

    test and code button click

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// // - String renders learn react link merupakan sebuah string untuk memberitahu
+// // bagian mana yang error ketika dijalankan.
+// // - pada awal-awal test, sebenarnya ketika test dikosongkan dia akan tetap pass
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// test('renders learn react link with learn react words', () => {
+//   render(<App />);
+//   const linkElement = screen.getByRole('link', {name: 'Learn React'});
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('button has correct initial color', () => {
+  render(<App/>)
+  const colorButton = screen.getByRole('button', {name: "Change to blue"})
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+  // click button
+  fireEvent.click(colorButton);
+  // expect button tobe blue
+  expect(colorButton).toHaveStyle({backgroundColor : 'blue'})
+  // expect button text to be 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red')
+
+})
